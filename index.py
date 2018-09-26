1bc533b04fe00795010034d947e450a2fc9e3053
SamehDorgham <samehvb@gmail.com>
2018-09-26 22:25:51 +0200

    Third commit

--- index.py
+++ index.py
@@ -49,7 +49,7 @@ class MainApp(QMainWindow, form_class):
         self.btn_Update_Car_Info.clicked.connect(self.Update_Car_Info)
         self.btn_Delete_Car_Info.clicked.connect(self.Delete_Car_Info)
 
-        self.btn_Add_Fuel_Info.clicked.connect(self.Add_Fuel_Info)
+        self.btn_Add_Fuel_Info.clicked.connect(self.Update_Fuel_Info)
         self.btn_Update_Fuel_Info.clicked.connect(self.Update_Fuel_Info)
 
         self.btn_Add_Maintenance_Info.clicked.connect(self.Add_Maintenance_Info)
@@ -72,19 +72,88 @@ class MainApp(QMainWindow, form_class):
 
 
     ###############################################################
-    ############## Add Search Car  ##############
+    ############## Add Search Car ... For Every Tab ##############
 
     def Search(self):
-        print("Search")
-        print(self.listWidget.row(self.listWidget.currentItem()))
 
-        sql =''' SELECT * FROM car_info WHERE car_number = %'''
-        car_number = self.txt_Search_For_CarData_By_CarNo()
-        self.cursor.execute(sql,[(car_number)])
+        ######################## Tab 0 ###########################
+
+        if self.listWidget.row(self.listWidget.currentItem()) == 0:
+            sql = ''' SELECT * FROM car_info WHERE car_number = %s'''
+            car_number = self.txt_Search_For_CarData_By_CarNo.text()
+
+            self.cursor.execute(sql,[(car_number)])
+            data = self.cursor.fetchall()
+
+            for row in data:
+                print(row)
+
+                self.txt_Car_Car_Number.setText(str(row[1]))
+                self.txt_Car_Owner_Company.setText(row[2])
+                self.txt_Car_Branch.setText(row[3])
+                self.cbo_Car_Service_Mode.setCurrentIndex(int(row[4]))
+                self.txt_Car_Shaceh_Number.setText(row[5])
+                self.txt_Car_Motor_Number.setText(row[6])
+                self.cbo_Car_Fuel_Type.setCurrentIndex(int(row[7]))
+                self.txt_Car_Car_Type.setText(row[8])
+                self.txt_Car_Car_Model.setText(str(row[9]))
+                self.txt_Car_Car_Load.setText(str(row[10]))
+                self.txt_Car_Car_Wieght.setText(str(row[11]))
+                self.txt_Car_Car_Shape.setText(row[12])
+                self.txt_Car_Car_Color.setText(row[13])
+
+
+        ######################## Tab 1 ###########################
+
+        if self.listWidget.row(self.listWidget.currentItem()) == 1:
+
+            sql = ''' SELECT * FROM fuel_info WHERE car_number = %s'''
+            car_number = self.txt_Search_For_CarData_By_CarNo.text()
+
+            self.cursor.execute(sql, [(car_number)])
+
+            data = self.cursor.fetchall()
+
+            for row in data:
+                print(row)
+                self.liter_number.setText(str(row[1]))
+                self.meter_reading.setText(str(row[2]))
+                self.tips.setText(str(row[3]))
+                self.liter_price.setText(str(row[4]))
+                self.lcdtotal_fuel.display((row[5]))
 
 
 
 
+        ######################## Tab 2 ###########################
+
+        if self.listWidget.row(self.listWidget.currentItem()) == 2:
+            print("tab1")
+
+
+
+
+        ######################## Tab 3 ###########################
+
+
+        if self.listWidget.row(self.listWidget.currentItem()) == 3:
+            print("tab1")
+
+
+
+
+        ######################## Tab 4 ###########################
+
+        if self.listWidget.row(self.listWidget.currentItem()) == 4:
+            print("tab1")
+
+
+
+
+        ######################## Tab 5 ###########################
+
+        if self.listWidget.row(self.listWidget.currentItem()) == 5:
+            print("tab1")
 
 
 
@@ -93,7 +162,6 @@ class MainApp(QMainWindow, form_class):
     ############## Car Info  ##############
 
     # Add Car
-
     def Add_Car_Info(self):
         car_number = self.txt_Car_Car_Number.text()
         owner_company = self.txt_Car_Owner_Company.text()
@@ -158,18 +226,91 @@ class MainApp(QMainWindow, form_class):
 
     def Update_Car_Info(self):
 
-        '''
-        self.cursor.execute(""" UPDATE car_info SET car_number=%s,owner_company =%s,branch=%s,service_mode=%s,shaceh_number=%s,motor_number=%s,fuel_type=%s,car_type=%s,car_model=%s,car_load=%s,car_weight=%s,car_shape=%s,car_color=%s
-        WHERE car_number=%s """, (car_number, owner_company, branch, service_mode, shaceh_number, motor_number, fuel_type, car_type, car_model,
-        car_load, car_weight, car_shape, car_color, car_number))
+        car_number = self.txt_Car_Car_Number.text()
+        owner_company = self.txt_Car_Owner_Company.text()
+        branch = self.txt_Car_Branch.text()
+        service_mode = self.cbo_Car_Service_Mode.currentIndex()
+        shaceh_number = self.txt_Car_Shaceh_Number.text()
+        motor_number = self.txt_Car_Motor_Number.text()
+        fuel_type = self.cbo_Car_Fuel_Type.currentIndex()
+        car_type = self.txt_Car_Car_Type.text()
+        car_model = self.txt_Car_Car_Model.text()
+        car_load = self.txt_Car_Car_Load.text()
+        car_weight = self.txt_Car_Car_Wieght.text()
+        car_shape = self.txt_Car_Car_Shape.text()
+        car_color = self.txt_Car_Car_Color.text()
 
-        '''
+        # Execute the SQL command
+
+        self.cursor.execute(
+            """ UPDATE car_info SET car_number = %s,owner_company = %s,branch= %s,service_mode = %s,shaceh_number = %s,
+            motor_number = %s,fuel_type = %s,car_type = %s,car_model = %s,car_load = %s,car_weight =%s,car_shape=%s,
+            car_color=%s WHERE car_number=%s """,
+            (car_number, owner_company, branch, service_mode, shaceh_number, motor_number, fuel_type, car_type,car_model, car_load, car_weight, car_shape, car_color,car_number))
+
+
+        self.cnx.commit()
+
+
+
+        # Erase All Controls Contents
+
+        self.txt_Car_Car_Number.setText("")
+        self.txt_Car_Owner_Company.setText("")
+        self.txt_Car_Branch.setText("")
+        self.cbo_Car_Service_Mode.setCurrentIndex(0)
+        self.txt_Car_Shaceh_Number.setText("")
+        self.txt_Car_Motor_Number.setText("")
+        self.cbo_Car_Fuel_Type.setCurrentIndex(0)
+        self.txt_Car_Car_Type.setText("")
+        self.txt_Car_Car_Model.setText("")
+        self.txt_Car_Car_Load.setText("")
+        self.txt_Car_Car_Wieght.setText("")
+        self.txt_Car_Car_Shape.setText("")
+        self.txt_Car_Car_Color.setText("")
+
+        # Show Successfully Add Data into Tables on StatusBar
+
+        self.statusbar.showMessage("تم تعديل البيانات بنجاح")
+
 
 
     # Delete Car
 
     def Delete_Car_Info(self):
-        pass
+
+        car_number = self.txt_Search_For_CarData_By_CarNo.text()
+
+        self.cursor.execute(""" DELETE FROM car_info WHERE car_number = %s""", (car_number,))
+
+        self.cursor.execute(""" DELETE FROM fuel_info WHERE car_number = %s""", (car_number,))
+        self.cursor.execute(""" DELETE FROM licence_info WHERE car_number = %s""", (car_number,))
+        self.cursor.execute(""" DELETE FROM maintenance_info WHERE car_number = %s""", (car_number,))
+        self.cursor.execute(""" DELETE FROM rent_info WHERE car_number = %s""", (car_number,))
+        self.cursor.execute(""" DELETE FROM revenue_info WHERE car_number = %s""", (car_number,))
+        self.cursor.execute(""" DELETE FROM WAEL_info WHERE car_number = %s""", (car_number,))
+
+        self.cnx.commit()
+
+        self.statusbar.showMessage("تم حذف البيانات بنجاح")
+
+        # Erase All Controls Contents
+
+        self.txt_Car_Car_Number.setText("")
+        self.txt_Car_Owner_Company.setText("")
+        self.txt_Car_Branch.setText("")
+        self.cbo_Car_Service_Mode.setCurrentIndex(0)
+        self.txt_Car_Shaceh_Number.setText("")
+        self.txt_Car_Motor_Number.setText("")
+        self.cbo_Car_Fuel_Type.setCurrentIndex(0)
+        self.txt_Car_Car_Type.setText("")
+        self.txt_Car_Car_Model.setText("")
+        self.txt_Car_Car_Load.setText("")
+        self.txt_Car_Car_Wieght.setText("")
+        self.txt_Car_Car_Shape.setText("")
+        self.txt_Car_Car_Color.setText("")
+
+
 
     ###############################################################
     ###############################################################
@@ -177,13 +318,30 @@ class MainApp(QMainWindow, form_class):
 
     # Add Fuel
 
-    def Add_Fuel_Info(self):
-        pass
-
-    # Update Fuel
-
     def Update_Fuel_Info(self):
-        pass
+
+        car_number = self.txt_Search_For_CarData_By_CarNo.text()
+
+        liter_number = float(self.liter_number.text())
+        meter_reading = float(self.meter_reading.text())
+        tips = float(self.tips.text())
+        liter_price = float(self.liter_price.text())
+
+        total = (liter_number * liter_price) + tips
+
+        self.lcdtotal_fuel.display(total)
+
+        # Execute the SQL command
+
+        self.cursor.execute("""UPDATE fuel_info SET liter_number = %s,counter_reading = %s,tips = %s,liter_price = %s,total = %s WHERE car_number = %s"""
+                            ,(liter_number, meter_reading, tips, liter_price, total, car_number))
+
+        self.cnx.commit()
+
+        self.statusbar.showMessage("تم إضافة المعلومات بنجاح")
+
+
+
 
     ###############################################################
     ############## Maintenance Info  ##############
